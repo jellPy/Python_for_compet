541443742481b58f997d8dcb9468cf87fb9ec94c
jellPy <kortoniks@mail.ru>
2025-09-11 19:53:01 +0300

    second day commit

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel
+from fastapi import Body
 from fastapi import FastAPI, status   # импортируем фреймворк и константы
 
 app = FastAPI()                       # создаём приложение
@@ -10,3 +12,20 @@ def ping() -> dict[str, bool]:
     GET /ping  →  {"pong": true}
     """
     return {"pong": True}
+
+class Echo(BaseModel):
+    message: str
+    times: int = 1
+
+@app.post("/echo", status_code=status.HTTP_201_CREATED)
+def echo(payload: Echo = Body(...)):
+    """
+    Возвращает сообщение `times` раз.
+    POST /echo
+    {
+      "message": "hi",
+      "times": 3
+    }
+    → ["hi", "hi", "hi"]
+    """
+    return [payload.message] * payload.times
